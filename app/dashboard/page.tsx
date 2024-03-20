5128783c28cb898b979f6f47082e1a27e5405abd
DavidCraftDev <contact@davidcraft.de>
2024-03-20 07:23:51 +0100

    Update Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,13 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/src/modules/auth";
 import { redirect } from "next/navigation";
+import QRScanner from "./events/event/qr.component";
+import { getSesession } from "../src/modules/authUtilities";
 
 export default async function Dashboard() {
-    const session = await getServerSession(authOptions);
-    if (!session) {
-        redirect("/api/auth/signin");
-    }
+    const session = await getSesession();
     return (
-        <div className="text-black">
+        <div>
             <h1>Dashboard</h1>
             <p>Hallo { session.user.name }</p>
         </div>
