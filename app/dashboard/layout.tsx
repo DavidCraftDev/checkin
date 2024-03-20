0ceed1ef691db0e39506b3ec7c8c35c3202ab699
DavidCraftDev <contact@davidcraft.de>
2024-03-20 07:23:17 +0100

    Make the Sidenav Mobile Friendly and add Permission Check

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,17 +1,13 @@
 import SideNav from '@/app/src/ui/sidenav';
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/src/modules/auth";
-import { redirect } from "next/navigation";
+import { getSesessionUser } from '../src/modules/authUtilities';
  
 export default async function Layout({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    redirect("/api/auth/signin");
-  }
+  const user = await getSesessionUser();
+  const permission = user.permission;
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-white">
       <div className="w-full flex-none md:w-64">
-        <SideNav />
+        <SideNav permission={permission}/>
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
--- app/src/ui/nav-links.tsx
+++ app/src/ui/nav-links.tsx
@@ -15,21 +15,22 @@ import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Übersicht', href: '/dashboard', icon: HomeIcon },
-  { name: 'QR-Code', href: '/dashboard/qrcode', icon: QrCodeIcon },
-  { name: 'Teilgenomme Veranstalltungen', href: '/dashboard/events/attendedEvents', icon: CalendarDaysIcon },
-  { name: 'Erstellte Veranstalltungen', href: '/dashboard/events/createdEvents', icon: PlusCircleIcon },
-  { name: 'Meine Gruppe', href: '/dashboard/groups/group', icon: UsersIcon },
-  { name: 'Gruppen', href: '/dashboard/groups/groups', icon: UserGroupIcon },
-  { name: 'Nutzer', href: '/dashboard/user', icon: UserCircleIcon },
+  { name: 'Übersicht', href: '/dashboard', icon: HomeIcon, mobile: false, permission: 0 },
+  { name: 'QR-Code', href: '/dashboard/qrcode', icon: QrCodeIcon, mobile: true, permission: 0 },
+  { name: 'Teilgenomme Veranstalltungen', href: '/dashboard/events/attendedEvents', icon: CalendarDaysIcon, mobile: true, permission: 0 },
+  { name: 'Erstellte Veranstalltungen', href: '/dashboard/events/createdEvents', icon: PlusCircleIcon, mobile: true, permission: 1 },
+  { name: 'Meine Gruppe', href: '/dashboard/groups/group', icon: UsersIcon, mobile: true, permission: 1 },
+  { name: 'Gruppen', href: '/dashboard/groups/groups', icon: UserGroupIcon, mobile: false, permission: 2 },
+  { name: 'Nutzer', href: '/dashboard/user', icon: UserCircleIcon, mobile: false, permission: 2 },
 ];
 
-export default function NavLinks() {
+export default async function NavLinks({ permission }: { permission: number }) {
 const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
+        if(permission < link.permission) return;
         return (
           <a
             key={link.name}
@@ -38,6 +39,7 @@ const pathname = usePathname();
                 'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
                 {
                     'bg-sky-100 text-blue-600': pathname === link.href,
+                    "hidden md:flex": !link.mobile,
                 },
             )}
           >
