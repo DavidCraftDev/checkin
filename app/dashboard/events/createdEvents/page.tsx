7f1abc06ea2e4c0f7202d498e34467d584782a78
DavidCraftDev <contact@davidcraft.de>
2024-03-25 15:55:29 +0100

    Design Change

--- app/dashboard/events/createdEvents/page.tsx
+++ app/dashboard/events/createdEvents/page.tsx
@@ -25,30 +25,29 @@ export default async function createdEvents({searchParams}: {searchParams: Searc
       <div>von { sessionUser.displayname }</div>
       <CreatEventButton />
       <CalendarWeek searchParams={searchParams} />
-      <div className="w-full mt-4 p-2 pb-0 border-gray-200 border-2 rounded-md">
-      <table className="table-auto w-full text-left">
+      <div className="table">
+      <table>
           <thead>
-              <tr className="border-b border-gray-600">
-                  <th className="py-4 px-2">Name</th>
-                  <th className="py-4 px-2">Teilnehmer</th>
-                  <th className="py-4 px-2">Zeit</th>
-                  <th className="py-4 px-2">Anzeigen</th>
+              <tr>
+                  <th>Name</th>
+                  <th>Teilnehmer</th>
+                  <th>Zeit</th>
+                  <th>Anzeigen</th>
               </tr>
           </thead>
           <tbody>
           {data.map((event: any) => (
-              <tr key={event.event.id} className="border-b border-gray-200">
-                  <td className="p-2">{event.event.name}</td>
-                  <td className="p-2">{event.user}</td>
-                  <td className="p-2">{moment(Date.parse(event.event.created_at)).format("DD.MM.YYYY HH:mm")}</td>
-                  <td className="p-2"><a href={`/dashboard/events/event?id=${event.event.id}`} className="hover:underline">Anzeigen</a></td>
+              <tr key={event.event.id}>
+                  <td>{event.event.name}</td>
+                  <td>{event.user}</td>
+                  <td>{moment(Date.parse(event.event.created_at)).format("DD.MM.YYYY HH:mm")}</td>
+                  <td><a href={`/dashboard/events/event?id=${event.event.id}`} className="hover:underline">Anzeigen</a></td>
               </tr>
           ))}
           </tbody>
       </table>
       </div>
       <p>Export Soon™</p>
-      <CreateEventModal/>
   </div>
     );
 }
--- app/dashboard/user/userTable.component.tsx
+++ app/dashboard/user/userTable.component.tsx
@@ -1,28 +1,26 @@
 "use client";
 
-import { User } from "@prisma/client";
-
 function UserTable(users: any) {
     return (
-        <div className="w-full mt-4 p-2 pb-0 border-gray-200 border-2 rounded-md overflow-y-scroll">
-        <table className="table-auto w-full text-left">
+        <div className="table">
+        <table>
             <thead>
-                <tr className="border-b border-gray-600">
-                    <th className="py-4 px-2">Name</th>
-                    <th className="py-4 px-2">Benutzername</th>
-                    <th className="py-4 px-2">Rechte</th>
-                    <th className="py-4 px-2">Gruppe</th>
-                    <th className="py-4 px-2">Bearbeiten</th>
+                <tr>
+                    <th>Name</th>
+                    <th>Benutzername</th>
+                    <th>Rechte</th>
+                    <th>Gruppe</th>
+                    <th>Bearbeiten</th>
                 </tr>
             </thead>
             <tbody>
             {users.users.map((user: any) => (
-                <tr key={user.id} className="border-b border-gray-200">
-                    <td className="p-2">{user.displayname}</td>
-                    <td className="p-2">{user.username}</td>
-                    <td className="p-2">{user.permission}</td>
-                    <td className="p-2">{user.group}</td>
-                    <td className="p-2"><a href={`/dashboard/user/edit?userID=${user.id}`} className="hover:underline">Bearbeiten</a></td>
+                <tr key={user.id}>
+                    <td>{user.displayname}</td>
+                    <td>{user.username}</td>
+                    <td>{user.permission}</td>
+                    <td>{user.group}</td>
+                    <td><a href={`/dashboard/user/edit?userID=${user.id}`} className="hover:underline">Bearbeiten</a></td>
                 </tr>
             ))}
             </tbody>
