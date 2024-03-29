d3a4f1f734aba8849966adacdea15ca010e96324
DavidCraftDev <contact@davidcraft.de>
2024-03-29 12:23:08 +0100

    Fix Logout Fehler

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,7 +1,10 @@
 import { signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default async function SignOut() {
-  signOut();
-  redirect("/api/auth/signin")
+  useEffect(() => {
+    signOut();
+    redirect("/api/auth/signin")
+  })
 }
