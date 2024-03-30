16c575a3e49c6e1910e65451b6addb12976d123f
DavidCraftDev <contact@davidcraft.de>
2024-03-30 19:23:55 +0100

    Fix multiple Event Creation

--- app/dashboard/events/createdEvents/submitHandler.ts
+++ app/dashboard/events/createdEvents/submitHandler.ts
@@ -4,13 +4,13 @@ import { getSesessionUser } from "@/app/src/modules/authUtilities";
 import { createEvent } from "@/app/src/modules/eventUtilities";
 import { redirect } from "next/navigation";
 
-let disabled = false;
+let lastResult: string = "";
 
 export default async function submitHandler(formData: FormData) {
-    if (disabled) return;
     if (!formData.get("name")) return;
-    disabled = true;
     const name: string = String(formData.get("name"));
+    if(name === lastResult) return;
+    lastResult = name;
     const sessionUser = await getSesessionUser(1);
     const userID: string = sessionUser.id || "";
     const data = await createEvent(name, userID)
