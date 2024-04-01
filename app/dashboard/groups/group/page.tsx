21e008349f096b47d530b6d7d677434ee9c0d0df
DavidCraftDev <contact@davidcraft.de>
2024-04-01 19:17:07 +0200

    Change Group Table Design

--- app/dashboard/groups/group/page.tsx
+++ app/dashboard/groups/group/page.tsx
@@ -4,6 +4,7 @@ import moment from "moment";
 import { getGroupMembers } from "@/app/src/modules/groupUtilities";
 import CalendarWeek from "@/app/src/ui/calendarweek";
 import { SearchParams } from "@/app/src/interfaces/searchParams";
+import GroupTable from "./groupTable.component";
 
 export default async function group({searchParams}: {searchParams: SearchParams}) {
   const sessionUser = await getSesessionUser(1);
@@ -22,29 +23,14 @@ export default async function group({searchParams}: {searchParams: SearchParams}
     const gruppenMitglieder = groupData.length;
     return (
       <div>
-      <h1>Gruppe {groupID}</h1>
-      <p>{gruppenMitglieder} Mitglieder</p>
-      <CalendarWeek searchParams={searchParams} />
-      <div className="w-full mt-4 p-2 pb-0 border-gray-200 border-2 rounded-md">
-      <table className="table-auto w-full text-left">
-          <thead>
-              <tr className="border-b border-gray-600">
-                  <th className="py-4 px-2">Name</th>
-                  <th className="py-4 px-2">Teilgenommene Events</th>
-                  <th className="py-4 px-2">Anzeigen</th>
-              </tr>
-          </thead>
-          <tbody>
-          {groupData.map((user: any) => (
-              <tr key={user.user.id} className="border-b border-gray-200">
-                  <td className="p-2">{user.user.displayname}</td>
-                  <td className="p-2">{user.attendances}</td>
-                  <td className="p-2"><a href={`/dashboard/events/attendedEvents?userID=${user.user.id}&cw=${cw}&year=${year}`} className="hover:underline">Anzeigen</a></td>
-              </tr>
-          ))}
-          </tbody>
-      </table>
-      </div>
+            <div className="grid grid-rows-1 grid-cols-1 md:grid-cols-2">
+            <div>
+                <h1>Gruppe {groupID}</h1>
+                <p>{gruppenMitglieder} Mitglieder</p>
+            </div>
+            <CalendarWeek searchParams={searchParams} />
+        </div>
+      <GroupTable user={groupData} cw={cw} year={year} />
       <p>Export Soon™</p>
   </div>
     );
