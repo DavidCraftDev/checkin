5c2a28b001679350fca9e9326ec03976b1ccf4b3
DavidCraftDev <contact@davidcraft.de>
2024-04-12 19:06:09 +0200

    Temp Test

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -4,8 +4,8 @@ export default function globalError({ error, reset }: { error: Error & { digest?
     return (
         <html>
             <body>
-                <h1>Es ist ein Fehler passiert!</h1>
-                <button onClick={() => reset()} className="btn bg-red-600">Erneut versuchen</button>
+                <h1>Es ist ein Fehler aufgetreten!</h1>
+                <button onClick={() => reset()} className="btn">Erneut versuchen</button>
                 <p>{"(" + String(error.digest) + ") " + String(error.message)}</p>
             </body>
         </html>
