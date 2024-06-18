0998c6fdc1d5401d6ce8a95785f9ec970ecf9da3
DavidCraftDev <contact@davidcraft.de>
2024-06-18 18:55:51 +0200

    Remove not needed awaits

--- app/src/ui/calendarweek.tsx
+++ app/src/ui/calendarweek.tsx
@@ -16,28 +16,28 @@ export async function addWeek(pathname: string, year: number, cw: number, router
     newCW++;
     newYear++;
     if (userID) {
-        if (String(cw) === "53") return await router.push(`${pathname}?year=${newYear}&cw=1&userID=${userID}`);
-        if (String(cw) !== "53") return await router.push(`${pathname}?year=${year}&cw=${newCW}&userID=${userID}`);
+        if (String(cw) === "53") return router.push(`${pathname}?year=${newYear}&cw=1&userID=${userID}`);
+        if (String(cw) !== "53") return router.push(`${pathname}?year=${year}&cw=${newCW}&userID=${userID}`);
     }
     if (groupID) {
-        if (String(cw) === "53") return await router.push(`${pathname}?year=${newYear}&cw=1&groupID=${groupID}`);
-        if (String(cw) !== "53") return await router.push(`${pathname}?year=${year}&cw=${newCW}&groupID=${groupID}`);
+        if (String(cw) === "53") return router.push(`${pathname}?year=${newYear}&cw=1&groupID=${groupID}`);
+        if (String(cw) !== "53") return router.push(`${pathname}?year=${year}&cw=${newCW}&groupID=${groupID}`);
     }
-    if (String(cw) === "53") return await router.push(`${pathname}?year=${newYear}&cw=1`);
-    if (String(cw) !== "53") return await router.push(`${pathname}?year=${year}&cw=${newCW}`);
+    if (String(cw) === "53") return router.push(`${pathname}?year=${newYear}&cw=1`);
+    if (String(cw) !== "53") return router.push(`${pathname}?year=${year}&cw=${newCW}`);
 }
 
 export async function subWeek(pathname: string, year: number, cw: number, router: AppRouterInstance, userID?: string, groupID?: string) {
     if (userID) {
-        if (String(cw) === "1") return await router.push(`${pathname}?year=${year - 1}&cw=53&userID=${userID}`);
-        if (String(cw) !== "1") return await router.push(`${pathname}?year=${year}&cw=${cw - 1}&userID=${userID}`);
+        if (String(cw) === "1") return router.push(`${pathname}?year=${year - 1}&cw=53&userID=${userID}`);
+        if (String(cw) !== "1") return router.push(`${pathname}?year=${year}&cw=${cw - 1}&userID=${userID}`);
     }
     if (groupID) {
-        if (String(cw) === "1") return await router.push(`${pathname}?year=${year - 1}&cw=53&groupID=${groupID}`);
-        if (String(cw) !== "1") return await router.push(`${pathname}?year=${year}&cw=${cw - 1}&groupID=${groupID}`);
+        if (String(cw) === "1") return router.push(`${pathname}?year=${year - 1}&cw=53&groupID=${groupID}`);
+        if (String(cw) !== "1") return router.push(`${pathname}?year=${year}&cw=${cw - 1}&groupID=${groupID}`);
     }
-    if (String(cw) === "1") return await router.push(`${pathname}?year=${year - 1}&cw=53`);
-    if (String(cw) !== "1") return await router.push(`${pathname}?year=${year}&cw=${cw - 1}`);
+    if (String(cw) === "1") return router.push(`${pathname}?year=${year - 1}&cw=53`);
+    if (String(cw) !== "1") return router.push(`${pathname}?year=${year}&cw=${cw - 1}`);
 }
 
 const CalendarWeek = ({ searchParams }: { searchParams: SearchParams }) => {
