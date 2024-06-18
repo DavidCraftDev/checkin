40957096376473861791f0f697b4dae1be2a7f53
DavidCraftDev <contact@davidcraft.de>
2024-06-18 16:47:09 +0200

    Improve default not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,6 +3,6 @@ import { getSessionUser } from "./src/modules/authUtilities";
 
 export default async function notFound() {
     const user = await getSessionUser();
-    if (user.id) redirect('/dashboard');
+    if (user && user.id) redirect('/dashboard');
     else redirect('/login');
 }
