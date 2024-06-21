b8ab01e59151de3adb3f80efcd53ef597e84c542
DavidCraftDev <contact@davidcraft.de>
2024-06-21 07:23:56 +0200

    Improve logout

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Logout() {
+  const { data: session } = useSession()
+  if (session == null) redirect("/login")
   useEffect(() => {
     signOut({ redirect: false });
-    redirect("/");
+    redirect("/login");
   }, []);
 }
