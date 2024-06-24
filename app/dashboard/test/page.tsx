baf6aea23e3855d5cd5b05ee4781625c9f6f5eda
DavidCraftDev <contact@davidcraft.de>
2024-06-24 08:27:29 +0200

    Fix

--- app/dashboard/test/page.tsx
+++ app/dashboard/test/page.tsx
@@ -3,6 +3,14 @@ import ldapClient from "@/app/src/modules/ldap";
 import { isLDAPEnabled } from "@/app/src/modules/ldapUtilities";
 
 export default async function TestPage() {
+    if(!isLDAPEnabled()) {
+        return (
+            <div>
+            <h1>Test Page</h1>
+            <p>LDAP is not enabled</p>
+            </div>
+        );
+    }
     let data = await getLdapClient();
     return (
         <div>
