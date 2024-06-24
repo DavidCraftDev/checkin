b2acce4e490b31f6620eea96d2e82a71839c6042
DavidCraftDev <contact@davidcraft.de>
2024-06-24 09:05:43 +0200

    New Test

--- app/src/modules/ldap.ts
+++ app/src/modules/ldap.ts
@@ -1,32 +1,35 @@
 import { Client, createClient } from 'ldapjs';
 import { isLDAPEnabled } from './ldapUtilities';
 
-let ldapClient: Client | null = null;
-
 console.log('LDAP enabled:', isLDAPEnabled())
 
 async function getLdapClient() {
-    if(!process.env.LDAP_URI && isLDAPEnabled()) throw new Error('LDAP_URI is required')
-        else if(!process.env.LDAP_URI?.startsWith('ldap://') && !process.env.LDAP_URI?.startsWith("ldaps://") && isLDAPEnabled()) throw new Error('LDAP_URI must start with ldap://')
-        else if(isLDAPEnabled()){
-            const tlsOptions = { 'rejectUnauthorized': false }
-            try {
+    if (!process.env.LDAP_URI && isLDAPEnabled()) throw new Error('LDAP_URI is required');
+    else if (!process.env.LDAP_URI?.startsWith('ldap://') && !process.env.LDAP_URI?.startsWith("ldaps://") && isLDAPEnabled()) throw new Error('LDAP_URI must start with ldap://');
+    else if (isLDAPEnabled()) {
+        const tlsOptions = { 'rejectUnauthorized': false };
+        let ldapClient;
+        try {
             ldapClient = createClient({
                 url: process.env.LDAP_URI as string,
                 reconnect: true,
                 tlsOptions: tlsOptions
             });
-            } catch (error) {
-                throw new Error('LDAP_URI is invalid: ' + error)
-            }
-            if(!process.env.LDAP_BIND_DN) return 'LDAP_BIND_DN is required' as string
-            if(!process.env.LDAP_BIND_CREDENTIALS) return 'LDAP_BIND_CREDENTIALS is required' as string
-            ldapClient.bind(process.env.LDAP_BIND_DN, process.env.LDAP_BIND_CREDENTIALS, (error: any) => {
-                if(error) return error as string
-                else return "LDAP bind successful" as string
-                ldapClient?.unbind()
-            })
+        } catch (error) {
+            throw new Error('LDAP_URI is invalid: ' + error);
         }
+        if (!process.env.LDAP_BIND_DN) return 'LDAP_BIND_DN is required'
+        if (!process.env.LDAP_BIND_CREDENTIALS) return 'LDAP_BIND_CREDENTIALS is required'
+        let data = await new Promise((resolve, reject) => {
+            ldapClient.bind(process.env.LDAP_BIND_DN || "ND", process.env.LDAP_BIND_CREDENTIALS || "ND", (error: any) => {
+                if (error) reject(error);
+                else resolve("LDAP bind successful");
+            });
+        });
+
+        ldapClient.unbind();
+        return "LDAP bind successful" + data;
+    }
 }
 
 export default getLdapClient
