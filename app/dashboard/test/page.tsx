9ce7589caafa397e1f065b1034972c5d16c4b86b
DavidCraftDev <contact@davidcraft.de>
2024-06-24 08:54:44 +0200

    Test

--- app/dashboard/test/page.tsx
+++ app/dashboard/test/page.tsx
@@ -15,6 +15,7 @@ export default async function TestPage() {
     return (
         <div>
         <h1>Test Page</h1>
+        <p>State: {process.env.ldap}</p>
         <p>{data}</p>
         </div>
     );
--- app/src/modules/ldap.ts
+++ app/src/modules/ldap.ts
@@ -21,13 +21,11 @@ async function getLdapClient() {
             }
             if(!process.env.LDAP_BIND_DN) return 'LDAP_BIND_DN is required' as string
             if(!process.env.LDAP_BIND_CREDENTIALS) return 'LDAP_BIND_CREDENTIALS is required' as string
-            let result: string = 'Binding to LDAP server...'
             ldapClient.bind(process.env.LDAP_BIND_DN, process.env.LDAP_BIND_CREDENTIALS, (error: any) => {
-                if(error) result = error as string
-                else result = "LDAP bind successful" as string
+                if(error) return error as string
+                else return "LDAP bind successful" as string
                 ldapClient?.unbind()
             })
-            return result
         }
 }
 
