a1d423047939f6eadd5f448859ec64c58b04ba96
DavidCraftDev <contact@davidcraft.de>
2024-06-24 16:49:03 +0200

    Health API endpoint

---
+++ app/health/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest } from "next/server";
+import db from "../src/modules/db";
+import { stat } from "fs";
+
+export async function GET(request: NextRequest) {
+    let dbConnected: Boolean
+    try {
+        await db.$connect()
+        await db.$queryRaw`SELECT 1`
+        dbConnected = true
+    } catch (error) {
+        dbConnected = false
+    }
+    let status = "ok"
+    if(!dbConnected ) {
+        status = "error"
+    }
+    const data = {
+        maintance: false,
+        status: status,
+        databaseConnected: dbConnected
+    }
+    return Response.json({ data })
+}
