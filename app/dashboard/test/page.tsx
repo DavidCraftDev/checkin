84ff2a68ac7410c3f8b7da8f8ed013b6c4c8c38b
DavidCraftDev <contact@davidcraft.de>
2024-06-26 15:17:54 +0200

    Better test site

--- app/dashboard/test/page.tsx
+++ app/dashboard/test/page.tsx
@@ -12,13 +12,13 @@ export default async function TestPage() {
         );
     }
     if(!process.env.example || !process.env.test) throw new Error("LDAP search filter and base are required");
-    let data = await search(process.env.example, process.env.test);
+    let data: any[] = await search(process.env.example, process.env.test);
     console.log(data)
     return (
         <div>
         <h1>Test Page</h1>
         <p>State: {process.env.ldap}</p>
-        <p>{String(data)}</p>
+        <p>{String(data[0].displayName)}</p>
         </div>
     );
 }
