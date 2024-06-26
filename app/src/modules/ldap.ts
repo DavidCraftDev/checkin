8bd64050d64a79c38c898d1e0fdf85e966c0686d
DavidCraftDev <contact@davidcraft.de>
2024-06-26 15:34:08 +0200

    Make it more logical

--- app/src/modules/ldap.ts
+++ app/src/modules/ldap.ts
@@ -3,6 +3,7 @@ import { isLDAPEnabled } from './ldapUtilities';
 
 let client: Client
 
+
 if (isLDAPEnabled()) {
     if (!process.env.LDAP_URI) throw new Error("LDAP_URI is required");
     if (!process.env.LDAP_BIND_DN) throw new Error("LDAP_BIND_DN is required");
@@ -19,22 +20,25 @@ if (isLDAPEnabled()) {
         throw new Error("Failed to create LDAP client: " + error);
     }
     console.log("LDAP client created successfully!")
-
+}
+async function bind() {
     console.log("Bind to LDAP Server...")
+    if(!process.env.LDAP_BIND_DN) throw new Error("LDAP_BIND_DN is required");
     try {
-        client.bind(process.env.LDAP_BIND_DN, process.env.LDAP_BIND_CREDENTIALS);
+        await client.bind(process.env.LDAP_BIND_DN, process.env.LDAP_BIND_CREDENTIALS);
         console.log("LDAP bind successful")
     } catch (error) {
         throw new Error("LDAP bind failed: " + error);
     }
 }
 
-export function unbind() {
-    client.unbind();
+export async function unbind() {
+    await client.unbind();
 }
 
 
 export async function search(filter: string, base: string) {
+    await bind();
     console.log("Searching LDAP server...")
     const { searchEntries, searchReferences } = await client.search(base, {
         filter: filter,
@@ -42,5 +46,6 @@ export async function search(filter: string, base: string) {
     console.log(searchEntries)
     console.log(searchReferences)
     console.log("LDAP search successful")
+    await unbind();
     return searchEntries
 }
