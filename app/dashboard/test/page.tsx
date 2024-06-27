a8739157e9cd4b11677b3f46578eb4d163331111
DavidCraftDev <contact@davidcraft.de>
2024-06-27 19:28:23 +0200

    Try it with a package

--- app/dashboard/test/page.tsx
+++ app/dashboard/test/page.tsx
@@ -1,6 +1,6 @@
-
-import { convertGUID, search } from "@/app/src/modules/ldap";
+import { search } from "@/app/src/modules/ldap";
 import { isLDAPEnabled } from "@/app/src/modules/ldapUtilities";
+import { UuidTool } from "uuid-tool";
 
 export default async function TestPage() {
     if(!isLDAPEnabled()) {
@@ -14,7 +14,7 @@ export default async function TestPage() {
     if(!process.env.example || !process.env.test) throw new Error("LDAP search filter and base are required");
     let data: any[] = await search(process.env.example, process.env.test);
     data.forEach(async (entry) => {
-        console.log(await convertGUID(entry.objectGUID))
+        console.log(UuidTool.toString(entry.objectGUID))
     });
     return (
         <div>
@@ -23,7 +23,7 @@ export default async function TestPage() {
         {data.map(async (entry) => {
             return (
                 <div key={entry.objectGUID}>
-                    <p>{await convertGUID(entry.objectGUID)}</p>
+                    <p>{UuidTool.toString(entry.objectGUID)}</p>
                     <p>{entry.sAMAccountName}</p>
                     <p>{entry.displayName}</p>
                     <p>{entry.pwdLastSet}</p>
