6c6cf1060aa051c7ec9ce7ef35c1a113c6dc71ff
DavidCraftDev <contact@davidcraft.de>
2024-06-28 15:45:51 +0200

    Try it

--- app/dashboard/test/page.tsx
+++ app/dashboard/test/page.tsx
@@ -14,7 +14,7 @@ export default async function TestPage() {
     if(!process.env.example || !process.env.test) throw new Error("LDAP search filter and base are required");
     let data: any[] = await search(process.env.example, process.env.test);
     data.forEach(async (entry) => {
-        console.log(await convertGUID(entry.objectGUID))
+        console.log(await convertGUID(entry.objectGUID[0]))
     });
     return (
         <div>
@@ -23,7 +23,7 @@ export default async function TestPage() {
         {data.map(async (entry) => {
             return (
                 <div key={entry.objectGUID}>
-                    <p>{await convertGUID(entry.objectGUID)}</p>
+                    <p>{await convertGUID(entry.objectGUID[0])}</p>
                     <p>{entry.sAMAccountName}</p>
                     <p>{entry.displayName}</p>
                     <p>{entry.pwdLastSet}</p>
