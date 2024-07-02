c93bbfb87c325f730e526fcf96c8a458f3c407c2
DavidCraftDev <contact@davidcraft.de>
2024-07-02 12:39:52 +0200

    Fix map error

--- app/dashboard/events/createdEvents/createEventForm.component.tsx
+++ app/dashboard/events/createdEvents/createEventForm.component.tsx
@@ -5,7 +5,7 @@ import { submitHandlerEvent, submitHandlerStudyTime } from "./submitHandler";
 function CreateEventForm(props: any) {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:grid-rows-1">
-            {props.studyTime ? <form action={submitHandlerStudyTime}>
+            {props.studyTime && props.user.competence.lenght !== 0 ? <form action={submitHandlerStudyTime}>
                 <div>
                     <label htmlFor="studyTime">Studienzeit</label><br />
                     <select defaultValue="default" id="studyTime" name="studyTime" className="rounded-full p-2 my-2 border-2 border-black-600 ring-0 ring-black-600 focus:outline-none focus:ring-1">
