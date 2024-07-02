62007dc65be205929e4533580bf9a0dfc46c2b4b
DavidCraftDev <contact@davidcraft.de>
2024-07-02 12:16:32 +0200

    Debug

--- scripts/ldapSeed.ts
+++ scripts/ldapSeed.ts
@@ -17,7 +17,7 @@ export async function seedLdapData(prisma: PrismaClient) {
             permission = { permission: 0 }
         }
         let group = {}
-        console.log(process.env.LDAP_AUTO_GROUPS)
+        console.log("Env:" + process.env.LDAP_AUTO_GROUPS as string)
         if (process.env.LDAP_AUTO_GROUPS === "true") {
             Promise.all(ldapUserData.memberOf.map(async (groupData: string) => 
                 {
