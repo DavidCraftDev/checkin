8db94f20b9c6173b913ab2a8ee89008ba5144ec5
DavidCraftDev <contact@davidcraft.de>
2024-07-02 12:48:31 +0200

    New Test Site

--- app/dashboard/test/page.tsx
+++ app/dashboard/test/page.tsx
@@ -12,21 +12,18 @@ export default async function TestPage() {
     if (!process.env.LDAP_SEARCH_BASE || !process.env.LDAP_SEARCH_BASE) throw new Error("LDAP search filter and base are required");
     let data: any[] = await getAllUsers();
     data.forEach(async (entry) => {
-        if (entry.sAMAccountName === "sascha.meyer") {
-            console.log(entry);
-        }
         entry.memberOf.map(async (group: string) => {
             let data = group.split(",")
             console.log(data[1].replace("OU=", "") + ": " + data[0].replace("CN=", ""))
         });
     });
-    let newData: any[] = await search("(cn=Team Oberstufe)", process.env.LDAP_SEARCH_BASE);
-    console.log(newData);
     return (
         <div>
             <h1>Test Page</h1>
             <p>State: {process.env.USE_LDAP}</p>
             {data.map(async (entry) => {
+
+                console.log(entry)
                 return (
                     <div key={entry.objectGUID}>
                         <p>{entry.objectGUID}</p>
