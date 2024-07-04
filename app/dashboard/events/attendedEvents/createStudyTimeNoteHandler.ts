1a4e243a120f81a2943e141d60648d7f8da43083
DavidCraftDev <contact@davidcraft.de>
2024-07-04 12:41:24 +0200

    Add missing disabled = false

--- app/dashboard/events/attendedEvents/createStudyTimeNoteHandler.ts
+++ app/dashboard/events/attendedEvents/createStudyTimeNoteHandler.ts
@@ -8,6 +8,7 @@ async function createNote(userID: string, cw: number) {
     if (disabled) return;
     disabled = true;
     await createUserStudyTimeNote(userID, cw);
+    disabled = false;
 }
 
 export default createNote;
