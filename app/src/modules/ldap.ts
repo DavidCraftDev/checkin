d1cc0057100065ae9f44a3eb87dc88304df943b0
DavidCraftDev <contact@davidcraft.de>
2024-07-10 17:20:08 +0200

    Push another bug

--- app/src/modules/ldap.ts
+++ app/src/modules/ldap.ts
@@ -11,7 +11,7 @@ if (use_ldap) {
     if (certExists) {
         tlsOptions = {
             rejectUnauthorized: ldap_tls_reject_unauthorized,
-            ca: [readFileSync("../../../cert.crt")]
+            ca: [readFileSync("../../../cert.crt").toString()]
         }
     } else {
         tlsOptions = {
