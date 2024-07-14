e4f3a598e85bdb450b0587b3de8607e173ad6924
DavidCraftDev <contact@davidcraft.de>
2024-07-14 15:49:17 +0200

    Fix Bug

--- app/dashboard/user/edit/userEditForm.component.tsx
+++ app/dashboard/user/edit/userEditForm.component.tsx
@@ -44,26 +44,26 @@ function UserEditForm(props: any) {
             <form action={handleSubmit} className="p-2">
                 <div>
                     <label htmlFor="displayname">Name*</label><br />
-                    <input type="text" name="displayname" id="displayname" placeholder="Max Mustermann" defaultValue={userData.displayname} className={clsx("rounded-full p-2 m-4 border-2 border-black-600 ring-0 ring-black-600 focus:outline-none focus:ring-1", { "border-red-600 ring-red-600": displaynameError }, { "disabled": config.use_ldap })} />
+                    <input type="text" name="displayname" id="displayname" placeholder="Max Mustermann" defaultValue={userData.displayname} disabled={config.use_ldap} className={clsx("rounded-full p-2 m-4 border-2 border-black-600 ring-0 ring-black-600 focus:outline-none focus:ring-1", { "border-red-600 ring-red-600": displaynameError })} />
                     <br/>
                     <label htmlFor="username">Nutzername*</label><br />
-                    <input type="text" name="username" id="username" placeholder="max.mustermann" defaultValue={userData.username} className={clsx("rounded-full p-2 m-4 border-2 border-black-600 ring-0 ring-black-600 focus:outline-none focus:ring-1", { "border-red-600 ring-red-600": usernameError }, { "disabled": config.use_ldap })} />
+                    <input type="text" name="username" id="username" placeholder="max.mustermann" defaultValue={userData.username} disabled={config.use_ldap} className={clsx("rounded-full p-2 m-4 border-2 border-black-600 ring-0 ring-black-600 focus:outline-none focus:ring-1", { "border-red-600 ring-red-600": usernameError })} />
                     <br />
                     <label htmlFor="permission">Rechte*</label><br />
-                    <select name="permission" id="permission" defaultValue={userData.permission} className={clsx("rounded-full p-2 m-4 border-2 bg-white border-black-600", { "disabled": config.ldap_auto_groups })} >
+                    <select name="permission" id="permission" defaultValue={userData.permission} disabled={config.ldap_auto_groups} className="rounded-full p-2 m-4 border-2 bg-white border-black-600">
                         <option value="0">Schüler</option>
                         <option value="1">Lehrer</option>
                         <option value="2">Admin</option>
                     </select>
                     <br />
                     <label htmlFor="group">Gruppe</label><br />
-                    <input type="text" name="group" id="group" placeholder="Klasse 14.2" defaultValue={userData.group} className={clsx("rounded-full p-2 m-4 border-2 border-black-600 ring-0 ring-black-600 focus:outline-none focus:ring-1", { "disabled": config.ldap_auto_permission })} />
+                    <input type="text" name="group" id="group" placeholder="Klasse 14.2" defaultValue={userData.group} disabled={config.ldap_auto_permission} className="rounded-full p-2 m-4 border-2 border-black-600 ring-0 ring-black-600 focus:outline-none focus:ring-1"/>
                     <br className={config.studyTime ? "" : "hidden"} />
                     <label htmlFor="needs" className={config.studyTime ? "" : "hidden"} >Benötigte Studienzeiten (Durch Komma getrennt)</label><br className={config.studyTime ? "" : "hidden"} />
-                    <input type="text" name="needs" id="needs" placeholder="Deutsch,Mathe,Englisch" defaultValue={userData.needs} className={clsx("rounded-full p-2 m-4 border-2 border-black-600 ring-0 ring-black-600 focus:outline-none focus:ring-1", { "hidden": !config.studyTime }, { "disabled": config.ldap_auto_studytime_data})} />
+                    <input type="text" name="needs" id="needs" placeholder="Deutsch,Mathe,Englisch" defaultValue={userData.needs} disabled={config.ldap_auto_studytime_data} className={clsx("rounded-full p-2 m-4 border-2 border-black-600 ring-0 ring-black-600 focus:outline-none focus:ring-1", { "hidden": !config.studyTime })} />
                     <br className={config.studyTime ? "" : "hidden"} />
                     <label htmlFor="competence" className={config.studyTime ? "" : "hidden"} >Kompetenzen (Durch Komma getrennt)</label><br className={config.studyTime ? "" : "hidden"} />
-                    <input type="text" name="competence" id="competence" placeholder="Deutsch,Mathe,Englisch" defaultValue={userData.competence} className={clsx("rounded-full p-2 m-4 border-2 border-black-600 ring-0 ring-black-600 focus:outline-none focus:ring-1", { "hidden": !config.studyTime }, { "disabled": config.ldap_auto_studytime_data})} />
+                    <input type="text" name="competence" id="competence" placeholder="Deutsch,Mathe,Englisch" defaultValue={userData.competence} disabled={config.ldap_auto_studytime_data} className={clsx("rounded-full p-2 m-4 border-2 border-black-600 ring-0 ring-black-600 focus:outline-none focus:ring-1", { "hidden": !config.studyTime })} />
                     <br className={config.use_ldap ? "hidden" : ""} />
                     <label htmlFor="password" className={config.use_ldap ? "hidden" : "" } >Neues Passwort setzen</label><br />
                     <input type="text" name="password" id="password" placeholder="Passwort" className={clsx("rounded-full p-2 m-4 border-2 border-black-600 ring-0 ring-black-600 focus:outline-none focus:ring-1", { "border-red-600 ring-red-600": passwordError }, { "hidden": config.use_ldap })} />
