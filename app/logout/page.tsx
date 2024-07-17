12e014aba76b36a5353bd75040ca6efbb380cc5a
DavidCraftDev <contact@davidcraft.de>
2024-07-17 18:46:46 +0200

    Improve logout page

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -4,11 +4,14 @@ import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-export default function Logout() {
+function Logout() {
   const router = useRouter()
   useEffect(() => {
     signOut({ redirect: false }).then(() => {
       router.push("/login");
     });
-  });
+  }, [router]);
+  return null;
 }
+
+export default Logout;
