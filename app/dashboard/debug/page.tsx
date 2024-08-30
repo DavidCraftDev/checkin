af7d80bb6dbae515c41314b5ac9a1360317d39d0
DavidCraftDev <contact@davidcraft.de>
2024-08-30 07:27:45 +0200

    LDAP Debug Page

---
+++ app/dashboard/debug/page.tsx
@@ -0,0 +1,15 @@
+import { use_ldap } from "@/app/src/modules/config";
+import { getAllUsers } from "@/app/src/modules/ldapUtilities";
+import { notFound } from "next/navigation";
+
+async function debugPage() {
+    if(!use_ldap) return notFound();
+    const data = await getAllUsers();
+    return (
+        <div>
+            <h1>Debug</h1>
+            <p>Debugging page</p>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+    );
+}
