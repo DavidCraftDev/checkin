3199573090c2e1c54bd962addf9bed79fa0abe8d
David <contact@davidcraft.de>
2024-08-30 08:00:00 +0200

    Update page.tsx

--- app/dashboard/debug/page.tsx
+++ app/dashboard/debug/page.tsx
@@ -2,7 +2,7 @@ import { use_ldap } from "@/app/src/modules/config";
 import { getAllUsers } from "@/app/src/modules/ldapUtilities";
 import { notFound } from "next/navigation";
 
-export async function debugPage() {
+export default async function debugPage() {
     if(!use_ldap) return notFound();
     const data = await getAllUsers();
     return (
