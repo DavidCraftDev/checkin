efa8d91717f60f1411ae2ded3416c5c00d8589fc
DavidCraftDev <contact@davidcraft.de>
2024-09-14 19:27:44 +0200

    Update attended events to remove normal events

--- app/dashboard/events/attendedEvents/attendedEventsTable.component.tsx
+++ app/dashboard/events/attendedEvents/attendedEventsTable.component.tsx
@@ -1,13 +1,12 @@
 "use client"
 
-import moment from "moment";
 import StudentNote from "./studentNote.component";
 import StudyTimeSelect from "./studyTimeSelect.component";
 import { AttendancePerUserPerEvent } from "@/app/src/interfaces/events";
+import dayjs from "dayjs";
 
 interface AttendedEventTableProps {
     attendances: AttendancePerUserPerEvent[];
-    studyTime: boolean;
     addable: boolean;
     studyTimeTypes: Record<string, string[]>;
 }
@@ -19,9 +18,9 @@ function AttendedEventTable(props: AttendedEventTableProps) {
                 <table>
                     <thead>
                         <tr>
-                            <th>Name</th>
+                            <th>Stammfach</th>
                             <th>Lehrer</th>
-                            {props.studyTime ? <th>Studienzeit</th> : null}
+                            <th>Studienzeit</th>
                             <th>Schüler Notiz</th>
                             <th>Lehrer Notiz</th>
                             <th>Wann Teilgenommen</th>
@@ -30,17 +29,17 @@ function AttendedEventTable(props: AttendedEventTableProps) {
                     <tbody>
                         {props.attendances.map((attendances: AttendancePerUserPerEvent) => (
                             <tr key={attendances.attendance.id}>
-                                <td>{attendances.event.name.replace(attendances.eventUser.displayname, "")}</td>
+                                <td>{attendances.event.type}</td>
                                 <td>{attendances.eventUser.displayname}</td>
-                                {props.studyTime ? attendances.event.studyTime ? props.addable ? <StudyTimeSelect attendance={attendances.attendance} studyTimeTypes={props.studyTimeTypes[attendances.attendance.id]} /> : attendances.attendance.type ? <td>{attendances.attendance.type.replace("parallel:", "Vertretung:").replace("note:", "Notiz:")}</td> : <span className={"italic"}>Keine Studienzeit ausgewählt</span> : <td>❌</td> : null}
+                                {props.addable ? <StudyTimeSelect attendance={attendances.attendance} studyTimeTypes={props.studyTimeTypes[attendances.attendance.id]} /> : attendances.attendance.type ? <td>{attendances.attendance.type}</td> : <span className={"italic"}>Keine Studienzeit ausgewählt</span>}
                                 {props.addable ? <StudentNote attendance={attendances.attendance} /> : <td>{attendances.attendance.studentNote}</td>}
                                 <td>{attendances.attendance.teacherNote}</td>
-                                <td>{moment(attendances.attendance.created_at).format("DD.MM.YYYY HH:mm")}</td>
+                                <td>{dayjs(attendances.attendance.created_at).format("DD.MM. HH:mm")}</td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
-                {props.attendances.length === 0 ? <p className="text-center italic m-2">An keiner Veranstaltungen teilgenommen</p> : null}
+                {props.attendances.length === 0 ? <p className="text-center italic m-2">An keiner Studienzeit teilgenommen</p> : null}
             </div>
         </div>
     )
--- app/dashboard/events/attendedEvents/createStudyTimeNoteHandler.ts
+++ app/dashboard/events/attendedEvents/createStudyTimeNoteHandler.ts
@@ -1,14 +1,18 @@
 "use server"
 
+import { disabledType } from "@/app/src/interfaces/utilties";
 import { createUserStudyTimeNote } from "@/app/src/modules/studytimeUtilities";
 
-let isDisabled: boolean = false;
+let disabled: disabledType = {};
 
 async function createStudyTimeNote(userID: string, cw: number) {
-    if (isDisabled) return;
-    isDisabled = true;
-    await createUserStudyTimeNote(userID, cw);
-    isDisabled = false;
+    if (disabled[userID] && disabled[userID] + 10000 > Date.now()) return;
+    disabled[userID] = Date.now();
+    try {
+        await createUserStudyTimeNote(userID, cw);
+    } finally {
+        delete disabled[userID]
+    }
 }
 
 export default createStudyTimeNote;
