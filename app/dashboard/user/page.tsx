e5d98f306d1746122669212a2386dbdb469eeec8
DavidCraftDev <contact@davidcraft.de>
2024-09-15 07:30:41 +0200

    Remove not needed component

--- app/dashboard/user/page.tsx
+++ app/dashboard/user/page.tsx
@@ -1,7 +1,6 @@
 import { getSessionUser } from "@/app/src/modules/authUtilities";
 import db from "@/app/src/modules/db"
 import UserTable from "./userTable.component";
-import CreateUserButton from "./createUserButton.component";
 import { use_ldap } from "@/app/src/modules/config";
 
 async function user() {
@@ -15,7 +14,7 @@ async function user() {
                     <h1>Nutzer</h1>
                     <p>{users.length} Nutzer</p>
                 </div>
-                {!use_ldap ? <CreateUserButton /> : null}
+                {!use_ldap ? <a className="btn place-self-center" href={`/dashboard/user/create`}>Nutzer erstellen</a> : null}
             </div>
             <UserTable users={users} />
             <p>Exportieren als:
