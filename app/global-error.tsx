1be98d6cd408eb2b3b94d7909e59a64e1490eca6
DavidCraftDev <contact@davidcraft.de>
2025-01-03 16:50:17 +0100

    Simplify button click on global error page

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
 export default function globalError({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
     return (
         <html>
             <body>
                 <h1>Es ist ein Fehler aufgetreten!</h1>
-                <button onClick={() => reset()} className="btn">Erneut versuchen</button>
+                <button onClick={reset} className="btn">Erneut versuchen</button>
                 <p>{"(" + String(error.digest) + ") " + String(error.message)}</p>
             </body>
         </html>
