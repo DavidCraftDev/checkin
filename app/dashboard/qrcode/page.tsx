84b4cef1826ce33315423592676e551992987686
DavidCraftDev <contact@davidcraft.de>
2025-01-05 11:42:14 +0100

    Refactor QR code components for improved clarity and consistency

--- app/dashboard/qrcode/page.tsx
+++ app/dashboard/qrcode/page.tsx
@@ -1,20 +1,20 @@
 import { redirect } from "next/navigation";
-import QRCode from './qr.component';
-import { getCurrentSession } from "@/app/src/modules/auth/cookieManager";
+import QRCodeComponent from "./qr.component";
+import { getCurrentSession } from "../../src/modules/auth/cookieManager";
 
-async function qrcode() {
+async function QRCodePage() {
   const { user } = await getCurrentSession();
-  if (!user) redirect('/login');
+  if (!user) redirect("/login");
   return (
     <div>
       <h1>QR-Code</h1>
       <div className="text-center w-full md:w-1/3">
-        <QRCode data={`checkin://${user.id}`} />
+        <QRCodeComponent data={"checkin://" + user.id} />
         <p>{user.displayname}</p>
-        <p>{`Nutzername: ${user.username}`}</p>
+        <p>Nutzername: {user.username}</p>
       </div>
     </div>
   );
 }
 
-export default qrcode;
+export default QRCodePage;
