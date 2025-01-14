67fc16ecc7f23d3820130c70b6a69a9020783b93
DavidCraftDev <contact@davidcraft.de>
2025-01-14 15:37:57 +0100

    Small Bugfix

--- app/logout/route.ts
+++ app/logout/route.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { deleteSessionTokenCookie, getCurrentSession } from "../src/modules/auth/cookieManager";
 import { invalidateSession } from "../src/modules/auth/sessionManager";
+import { redirect } from "next/navigation";
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
     const { session } = await getCurrentSession();
     if (session) await invalidateSession(session.id);
     await deleteSessionTokenCookie();
-    return NextResponse.redirect(new URL("/login", request.url));
+    return redirect("/login");
 }
