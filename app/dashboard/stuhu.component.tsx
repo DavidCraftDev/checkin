47a4c43a3b4446a448851f77d95a24d392d850ab
DavidCraftDev <contact@davidcraft.de>
2025-04-01 19:13:22 +0200

    fix: adjust interval for StuhuTips component

--- app/dashboard/stuhu.component.tsx
+++ app/dashboard/stuhu.component.tsx
@@ -54,7 +54,7 @@ function StuhuTips() {
             if (audioRef.current) {
                 audioRef.current.play();
             }
-        }, Math.random() * 120000 + 10000);
+        }, Math.random() * 90000 + 10000);
 
         return () => clearInterval(interval);
     }, []);
