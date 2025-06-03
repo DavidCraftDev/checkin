69517ce96e831f06738ab8d64127ce60ae18cf84
DavidCraftDev <contact@davidcraft.de>
2025-06-03 15:11:29 +0200

    fix: ensure readConfig is called before redirecting in toggleSponsorenlaufModule

--- app/administration/modules/actions.ts
+++ app/administration/modules/actions.ts
@@ -2,11 +2,14 @@
 
 import { config_data, readConfig, writeConfig } from "@/app/src/modules/data/config";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/dist/client/components/navigation";
 
 export async function toggleSponsorenlaufModule(formData: FormData) {
     const newValue = formData.get("sponsorenlauf") === "on";
     readConfig(false);
     config_data.MODULES.SPONSORENLAUF = newValue;
     writeConfig();
+    readConfig(false);
     revalidatePath("/administration/modules");
+    redirect("/administration/modules");
 }
