b276f69462c7c4c1092c55cb51d93f966afd5a5e
DavidCraftDev <contact@davidcraft.de>
2025-06-03 16:00:43 +0200

    fix: reorder readConfig call in toggleSponsorenlaufModule for correct execution flow

--- app/administration/modules/actions.ts
+++ app/administration/modules/actions.ts
@@ -6,8 +6,8 @@ import { redirect } from "next/dist/client/components/navigation";
 
 export async function toggleSponsorenlaufModule(formData: FormData) {
     const newValue = formData.get("sponsorenlauf") === "on";
-    readConfig(false);
     config_data.MODULES.SPONSORENLAUF = newValue;
+    readConfig(false);
     writeConfig();
     readConfig(false);
     revalidatePath("/administration/modules");
