95ed494c371a6a0b7bcb4d05737b5bbaf5ae508f
DavidCraftDev <david@davidcraft.de>
2025-06-04 08:22:48 +0200

    fix: export deleteLesson as default function

--- app/dashboard/events/lesson/[slug]/delete/page.tsx
+++ app/dashboard/events/lesson/[slug]/delete/page.tsx
@@ -2,7 +2,7 @@ import db from "@/app/src/modules/db";
 import logger from "@/app/src/modules/logger";
 import { notFound, redirect } from "next/navigation";
 
-export async function deleteLesson({ params }: { params: Promise<{ slug: string }> }) {
+export default async function deleteLesson({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params
     if (!slug) return notFound();
     db.attendances.deleteMany({
