28b2ec68dd2c3508493307336f53267e356d9f9c
DavidCraftDev <david@davidcraft.de>
2025-08-14 14:55:25 +0200

    fix: correct spelling of 'TrafficLightFeedback' in migration and schema files

--- prisma/migrations/20250617064637_add_self_reflection_and_feedback_fields/migration.sql
+++ prisma/migrations/20250617064637_add_self_reflection_and_feedback_fields/migration.sql
@@ -1,7 +1,7 @@
 -- CreateEnum
-CREATE TYPE "TraficLightFeedback" AS ENUM ('GREEN', 'YELLOW', 'RED');
+CREATE TYPE "TrafficLightFeedback" AS ENUM ('GREEN', 'YELLOW', 'RED');
 
 -- AlterTable
-ALTER TABLE "Attendances" ADD COLUMN     "feedback" "TraficLightFeedback" NOT NULL DEFAULT 'GREEN',
+ALTER TABLE "Attendances" ADD COLUMN     "feedback" "TrafficLightFeedback" NOT NULL DEFAULT 'GREEN',
 ADD COLUMN     "goodAtmosphere" BOOLEAN NOT NULL DEFAULT true,
 ADD COLUMN     "productiveWork" BOOLEAN NOT NULL DEFAULT true;
