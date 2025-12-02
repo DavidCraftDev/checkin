f9b0a35b66bb47d96c54e1d5eaba0902959f4a15
DavidCraftDev <david@davidcraft.de>
2025-12-02 09:20:33 +0100

    Refactor LockedStudyTimes model to use lessonID as primary key and update migration accordingly

--- prisma/migrations/20251202081611_add_study_time_lock/migration.sql
+++ prisma/migrations/20251202081611_add_study_time_lock/migration.sql
@@ -1,11 +1,10 @@
 -- CreateTable
 CREATE TABLE "LockedStudyTimes" (
-    "id" TEXT NOT NULL,
     "lessonID" TEXT NOT NULL,
     "courseID" TEXT NOT NULL,
 
-    CONSTRAINT "LockedStudyTimes_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "LockedStudyTimes_pkey" PRIMARY KEY ("lessonID")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "LockedStudyTimes_id_key" ON "LockedStudyTimes"("id");
+CREATE UNIQUE INDEX "LockedStudyTimes_id_key" ON "LockedStudyTimes"("lessonID");
