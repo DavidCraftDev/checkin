b481078a0697d0c9de4d47832d1a884fbdf34ec8
DavidCraftDev <david@davidcraft.de>
2026-01-30 08:12:50 +0100

    fix: update study time label from "Keine Studienzeit" to "Zusätzliche Studienzeit"

--- app/dashboard/events/attendedEvents/page.tsx
+++ app/dashboard/events/attendedEvents/page.tsx
@@ -66,7 +66,7 @@ async function AttendedEventsPage(props: { searchParams: Promise<SearchParams> }
                 else vertretung.push(missingStudyTime);
             });
             vertretung.forEach((vertretung) => neededStudyTimesForAttendance.push("Vertretung:" + vertretung));
-            neededStudyTimesForAttendance.push("Keine Studienzeit");
+            neededStudyTimesForAttendance.push("Zusätzliche Studienzeit");
             if(isTeacher) neededStudyTimesForAttendance.push("Löschen");
             studyTimeTypes[event.attendance.id] = neededStudyTimesForAttendance;
         } else {
